db948e12a979db8d565378d8f3067a8a42a6607d
filipenasc <filipemateusn@gmail.com>
2016-01-13 15:09:20 -0200

    fixing tab spaces

--- admin/create-settings.php
+++ admin/create-settings.php
@@ -2,47 +2,47 @@
 
 add_action( 'init', 'groupper_custom_post_types' );
 function groupper_custom_post_types() {
-    $labels = array(
-        'name'                  => _x( 'Groupper', 'post_type_general_name' ),
-        'singular_name'         => _x( 'Groupper', 'post_type_singular_name' ),
-        'add_new'               => _x( 'Adicionar novo', 'integration' ),
-        'add_new_item'          => __( 'Adicionar nova lista' ),
-        'edit_item'             => __( 'Editar lista' ),
-        'new_item'              => __( 'Nova lista' ),
-        'all_items'             => __( 'Todas as listas' ),
-        'view_item'             => __( 'Ver lista' ),
-        'search_items'          => __( 'Procurar listas' ),
-        'not_found'             => __( 'Nenhuma lista encontrada' ),
-        'not_found_in_trash'    => __( 'Nenhuma lista encontrada na lixeira' ),
-        'parent_item_colon'     => '',
-        'menu_name'             => 'Groupper - Posts'
-    );
-    $args = array(
-        'labels'                => $labels,
-        'description'           => 'Create custom archives to WordPress posts',
-        'public'                => true,
-        'menu_position'         => 30,
-        'supports'              => array( 'title' ),
-        'has_archive'           => false,
-        'exclude_from_search'   => true,
-        'show_in_admin_bar'     => true,
-        'show_in_nav_menus'     => true,
-        'publicly_queryable'    => false,
-        'query_var'             => false
-    );
-    register_post_type( 'groupper_cpt', $args );
+  $labels = array(
+    'name'                  => _x( 'Groupper', 'post_type_general_name' ),
+    'singular_name'         => _x( 'Groupper', 'post_type_singular_name' ),
+    'add_new'               => _x( 'Adicionar novo', 'integration' ),
+    'add_new_item'          => __( 'Adicionar nova lista' ),
+    'edit_item'             => __( 'Editar lista' ),
+    'new_item'              => __( 'Nova lista' ),
+    'all_items'             => __( 'Todas as listas' ),
+    'view_item'             => __( 'Ver lista' ),
+    'search_items'          => __( 'Procurar listas' ),
+    'not_found'             => __( 'Nenhuma lista encontrada' ),
+    'not_found_in_trash'    => __( 'Nenhuma lista encontrada na lixeira' ),
+    'parent_item_colon'     => '',
+    'menu_name'             => 'Groupper - Posts'
+  );
+  $args = array(
+      'labels'                => $labels,
+      'description'           => 'Create custom archives to WordPress posts',
+      'public'                => true,
+      'menu_position'         => 30,
+      'supports'              => array( 'title' ),
+      'has_archive'           => false,
+      'exclude_from_search'   => true,
+      'show_in_admin_bar'     => true,
+      'show_in_nav_menus'     => true,
+      'publicly_queryable'    => false,
+      'query_var'             => false
+  );
+  register_post_type( 'groupper_cpt', $args );
 }
 
 
 add_action( 'add_meta_boxes', 'groupper_get_posts' );
 function groupper_get_posts() {
-    add_meta_box(
-        'groupper_get_posts',
-        'Posts do Blog',
-        'groupper_get_posts_content',
-        'groupper_cpt',
-        'normal'
-    );
+  add_meta_box(
+      'groupper_get_posts',
+      'Posts do Blog',
+      'groupper_get_posts_content',
+      'groupper_cpt',
+      'normal'
+  );
 }
 
 function groupper_get_posts_content(){
@@ -75,10 +75,10 @@ add_action('wp_ajax_show_posts_content', 'ajax_show_posts_content');
 function ajax_show_posts_content() {
 	$args = array( 'numberposts' => -1, 's' => $_GET['q'], 'post_type' => 'post');
 	$query = new WP_Query($args);
-    while ($query->have_posts()) : $query->the_post();
-    	echo "<p id='ajax-post-title' class='ajax-post-title'><span id='get-post-id'>".get_the_ID()."</span> - ".get_the_title()."</p> \n";
-    endwhile;
-    die();
+  while ($query->have_posts()) : $query->the_post();
+  	echo "<p id='ajax-post-title' class='ajax-post-title'><span id='get-post-id'>".get_the_ID()."</span> - ".get_the_title()."</p> \n";
+  endwhile;
+  die();
 }
 
 add_action('save_post', 'groupper_save_meta_boxes');
